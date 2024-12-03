14b3d7e276d53a1808f3571e159ee6a3a36176fd
Thais Goulart <thaisgoulartf@gmail.com>
2024-12-03 18:05:19 -0300

    changing project to typescript

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/thaisgoulartf.png',
+      name: 'Thais Goulart',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+
+      { type: 'link', content: '👉jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-11-21 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://www.petz.com.br/blog/wp-content/uploads/2022/10/gato-siames-muda-de-cor.jpg',
+      name: 'Croissant',
+      role: 'Gato Siamês'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+
+      { type: 'link', content: '👉jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-11-20 20:00:00')
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
